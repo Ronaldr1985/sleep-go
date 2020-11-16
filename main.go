599d7c2f84a8fb5fc6beab60637b03ef2ebaa607
Ronald <Ronald1985@tutamail.com>
2020-11-16 22:07:16 +0000

    Final worked out how to do milliseconds, still needs tuning but it works

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
 	"strings"
 	"time"
 )
@@ -28,51 +27,39 @@ func countdown(minutes, seconds int64) {
 }
 
 func main() {
-	timeRegex, _ := regexp.Compile("[0-9]")
-	checkhours := "h"
-	checkminutes := "m"
-	checkseconds := "s"
-	checkmilliseconds := "ms"
-	var count int = 0
+	// timeRegex, _ := regexp.Compile("[0-9]")
+	hours := "h"
+	minutes := "m"
+	seconds := "s"
+	milliseconds := "ms"
+	// var time []string
+	var denominator []string
+	// var denominatorplace []int
+	// var count int = 0
+	var previouscharacter string
 	if len(os.Args) > 1 {
 		for i, arg := range os.Args {
 			if i > 0 {
-				var time []string
-				var denominator []string
-				var denominatorplace []int
-				for i, x := range arg { // Loop through argument as individual strings
-					if timeRegex.MatchString(string(x)) == true { // Match string again numbers and add it to time array
-						x := string(x)
-						time = append(time, x)
-					} else { // if it doesn't match regex check if it's a millisecond, second, minute, hour
-						if strings.Contains(string(x), checkhours) == true || strings.Contains(string(x), checkminutes) == true || strings.Contains(string(x), checkseconds) || strings.Contains(string(x), checkmilliseconds) {
-							denominator = append(denominator, string(x))
-							denominatorplace = append(denominatorplace, i)
+				var count int = 0
+				for _, character := range arg {
+					if strings.Contains(string(character), seconds) {
+						if strings.Contains(previouscharacter, minutes) == true {
+							fmt.Println("Milliseconds were passed")
+							count++
+							if len(denominator) > 0 {
+								denominator = denominator[:len(denominator)-1]
+								denominator = append(denominator, milliseconds)
+							} else {
+								denominator = append(denominator, milliseconds)
+							}
 						}
 					}
-				}
-				fmt.Printf("Which means sleeping for %s%s\n", time[0:denominatorplace[0]], denominator[0:1])
-				fmt.Printf("Denominator is in place %d\n", denominatorplace[0:])
-				// Do case statement here to check whether sleeping for hours, minutes, seconds or milliseconds
-				fmt.Println(time)
-				for i, x := range denominator { // Loop through denominators
-					switch string(x) {
-					case "m":
-						fmt.Println("\nCount variable is equal to:", count)
-						fmt.Println("denominatorplace[i] variable is equal to:", denominatorplace[0])
-						fmt.Println("Arg variable is equal to:", arg)
-						fmt.Println("Variable i is equal to:", i)
-						fmt.Printf("Sleeping for %s minutes\n", arg[count:denominatorplace[i]])
-					case "s":
-						fmt.Println("\nCount variable is equal to:", count)
-						fmt.Println("denominatorplace[i] variable is equal to:", denominatorplace[0])
-						fmt.Println("Arg variable is equal to:", arg)
-						fmt.Println("Variable i is equal to:", i)
-						// var seconds string
-						// seconds = strings.Join(time[count:denominatorplace[0]], " ")
-						fmt.Printf("Sleeping for %s seconds\n", arg[(denominatorplace[count]+1):denominatorplace[i]])
+					if strings.Contains(string(character), hours) || strings.Contains(string(character), minutes) {
+						denominator = append(denominator, string(character))
 					}
+					previouscharacter = string(character)
 				}
+				fmt.Println(denominator)
 			}
 		}
 	} else {
